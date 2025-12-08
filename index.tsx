bc56525647ba8a4f01c8e404c29205d18c2fe8eb
jefferson22gs <jefferson.22gs@gmail.com>
2025-12-07 21:13:25 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -19,7 +19,7 @@ if ('serviceWorker' in navigator) {
   
   if (!isPreview) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./public/sw.js').then(registration => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
             console.log('SW registered: ', registration);
         }).catch(registrationError => {
             console.log('SW registration failed: ', registrationError);
